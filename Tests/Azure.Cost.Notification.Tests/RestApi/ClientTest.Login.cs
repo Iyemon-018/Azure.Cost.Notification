24ed93466c5f3b529ba010bcfcb21d110811c17f
石井 健太 <i_m_a_walker@live.jp>
2022-07-17 23:01:42 +0900

    add client tests

---
+++ Tests/Azure.Cost.Notification.Tests/RestApi/ClientTest.Login.cs
@@ -0,0 +1,58 @@
+﻿namespace Azure.Cost.Notification.Tests.RestApi;
+
+using System.Net;
+using System.Threading.Tasks;
+using Azure.RestApi.CostManagement.Requests;
+using ChainingAssertion;
+using Moq.Contrib.HttpClient;
+using Xunit;
+
+public partial class ClientTest
+{
+    [Fact]
+    public async Task Test_Login_GetAccessTokenAsync_200OKを返す場合に応答結果の値がレスポンスと一致すること()
+    {
+        _testFactory.Handler
+                    .SetupAnyRequest()
+                    .ReturnsResponse("{\"token_type\": \"Bearer\""
+                                   + ",\"expires_in\": \"3599\""
+                                   + ",\"ext_expires_in\": \"1599\""
+                                   + ",\"expires_on\": \"1656516529\""
+                                   + ",\"not_before\": \"1656512629\""
+                                   + ",\"resource\": \"https://management.core.windows.net/\""
+                                   + ",\"access_token\": \"<access_token>\"}"
+                           , "application/json");
+
+        var response = await _target.Login
+                                    .GetAccessTokenAsync("0000-0000-0000", AccessTokenRequestBody.AsClientCredentials("user-id", "xxxxx"));
+
+        response.IsSuccess.IsTrue();
+        response.StatusCode.Is(HttpStatusCode.OK);
+
+        response.Content.token_type.Is("Bearer");
+        response.Content.expires_in.Is(3599);
+        response.Content.ext_expires_in.Is(1599);
+        response.Content.expires_on.Is(1656516529);
+        response.Content.not_before.Is(1656512629);
+        response.Content.resource.Is("https://management.core.windows.net/");
+        response.Content.access_token.Is("<access_token>");
+
+        response.ErrorMessage.IsNull();
+    }
+
+    [Fact]
+    public async Task Test_Login_GetAccessTokenAsync_応答がエラーの場合にエラー情報を返すこと()
+    {
+        _testFactory.SetupErrorResponse();
+
+        var response = await _target.Login
+                                    .GetAccessTokenAsync("0000-0000-0000", AccessTokenRequestBody.AsClientCredentials("user-id", "xxxxxx"));
+
+        response.IsSuccess.IsFalse();
+        response.StatusCode.Is(HttpStatusCode.Unauthorized);
+        response.ErrorMessage.Is("AuthenticationFailed: Authentication failed.");
+
+        response.Content.IsNull();
+        response.RequestUri.AbsoluteUri.Is("https://login.microsoftonline.com/0000-0000-0000/oauth2/token");
+    }
+}
---
+++ Tests/Azure.Cost.Notification.Tests/RestApi/ClientTest.cs
@@ -0,0 +1,46 @@
+﻿namespace Azure.Cost.Notification.Tests.RestApi;
+
+using System.Net;
+using System.Net.Http;
+using Azure.RestApi.CostManagement;
+using Moq;
+using Moq.Contrib.HttpClient;
+
+public partial class ClientTest
+{
+    private readonly TestFactory _testFactory;
+
+    private readonly Client _target;
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// cf. https://github.com/maxkagamine/Moq.Contrib.HttpClient/blob/master/README.ja.md
+    /// </remarks>
+    public class TestFactory
+    {
+        public TestFactory()
+        {
+            Handler = new Mock<HttpMessageHandler>();
+        }
+
+        public Mock<HttpMessageHandler> Handler { get; }
+
+        public HttpClient CreateClient() => Handler.CreateClient();
+
+        public void SetupErrorResponse(HttpStatusCode statusCode = HttpStatusCode.Unauthorized
+                                     , string         code       = "AuthenticationFailed"
+                                     , string         message    = "Authentication failed.")
+            => Handler
+              .SetupAnyRequest()
+              .ReturnsResponse(HttpStatusCode.Unauthorized, $"{{ \"error\": {{\"code\": \"{code}\", \"message\": \"{message}\"}}}}");
+    }
+
+    public ClientTest()
+    {
+        _testFactory = new TestFactory();
+        _target      = new Client(_testFactory.CreateClient());
+    }
+}
